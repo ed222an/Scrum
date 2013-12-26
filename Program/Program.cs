cf6adc059d3df5d2488017005f9d39720260032a
Emil <ed222an@student.lnu.se>
2013-12-26 17:06:03 +0100

    Sprint 1 - Alla uppgifter färdigställda.

--- Program/Program.cs
+++ Program/Program.cs
@@ -12,15 +12,15 @@ namespace Program
         {
 
             // Exempellista med medlemmar.
-            Member[] memberList = new Member[4];
-            memberList[0] = new Member("Clark", "Kent", 1111111, 1);
-            memberList[1] = new Member("Bruce", "Wayne", 2222222, 2);
-            memberList[2] = new Member("Hal", "Jordan", 3333333, 3);
-            memberList[3] = new Member("Diana", "Prince", 4444444, 4);
+            List<Member> memberList = new List<Member>();
+
+            memberList.Add(new Member("Clark", "Kent", 1111111, 1));
+            memberList.Add(new Member("Bruce", "Wayne", 2222222, 2));
+            memberList.Add(new Member("Hal", "Jordan", 3333333, 3));
+            memberList.Add(new Member("Diana", "Prince", 4444444, 4));
 
             // Variabel för menyval.
             int option = 0;
-            Member chosenMember = null;
 
             while (true)
             {
@@ -40,13 +40,14 @@ namespace Program
                     case 2:
                         Console.Clear();
                         // Visa enskild medlem.
-                        chosenMember = GetMember(memberList);
-                        ShowMember(chosenMember);
+                        ShowMember(GetMember(memberList));
                         ContinueOnKeyPressed();
                         break;
                     case 3:
                         Console.Clear();
                         // Registrera ny medlem.
+                        memberList.Add(RegisterNewMember(memberList));
+                        Message("Ny medlem sparad");
                         ContinueOnKeyPressed();
                         break;
                     case 4:
@@ -54,12 +55,17 @@ namespace Program
                         // Ta bort medlem.
                         ContinueOnKeyPressed();
                         break;
+                    case 5:
+                        Console.Clear();
+                        // Redigera befintlig medlem.
+                        ContinueOnKeyPressed();
+                        break;
                 }
             }
 
         }
 
-        private static int GetMenuChoice() // Visar menyn och ber användaren mata in ett värde som sedan skickas tillbaks. KLAR!
+        private static int GetMenuChoice() // Visar menyn och ber användaren mata in ett värde som sedan skickas tillbaks.
         {
             int menuChoice = 0;
 
@@ -74,16 +80,16 @@ namespace Program
                 Console.WriteLine("\n - Arkiv -----------------------------------------\n");
                 Console.WriteLine(" 0. Avsluta.\n 1. Visa medlemslista.\n 2. Visa enskild medlem.\n");
                 Console.WriteLine(" - Redigera -------------------------------------- \n");
-                Console.WriteLine(" 3. Registrera ny medlem.\n 4. Ta bort medlem\n");
+                Console.WriteLine(" 3. Registrera ny medlem.\n 4. Ta bort medlem\n 5. Redigera befintlig medlem\n");
                 Console.WriteLine(" ═════════════════════════════════════════════════ \n\n");
-                Console.Write(" Ange menyval [0-4]: ");
+                Console.Write(" Ange menyval [0-5]: ");
 
                 try // Ber användaren mata in ett menyalternativ & testar ifall det inmatade värdet är något utav de tillåtna intervallet 0 - 3 .
                 {
                     menuChoice = int.Parse(Console.ReadLine());
                     Console.Clear();
 
-                    if (menuChoice < 0 || menuChoice > 4)
+                    if (menuChoice < 0 || menuChoice > 5)
                     {
                         throw new Exception();
                     }
@@ -97,7 +103,7 @@ namespace Program
                     Console.Clear();
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("\n FEL! Ange ett heltal mellan 0-3!\n\n");
+                    Console.WriteLine("\n FEL! Ange ett heltal mellan 0-5!\n\n");
                     Console.ResetColor();
                 }
             } while (true);
@@ -105,7 +111,7 @@ namespace Program
             return menuChoice;
         }
 
-        private static void ContinueOnKeyPressed() // Vid anrop bes användaren att trycka på valfri tangent för att fortsätta. KLAR!
+        private static void ContinueOnKeyPressed() // Vid anrop bes användaren att trycka på valfri tangent för att fortsätta.
         {
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.White;
@@ -115,22 +121,45 @@ namespace Program
             Console.Clear();
         }
 
-        private static void ShowMemberList(Member[] memberList) // Visar menyn och ber användaren mata in ett värde som sedan skickas tillbaks. KLAR!
+        private static void Message(string prompt)
         {
+            // Skriver ut medlemmens information.
             Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n ╔");
+            for (int i = 0; i < prompt.Length + 2; i++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("╗ \n");
+            Console.WriteLine(" ║ {0} ║ ", prompt);
+
+
+            Console.Write(" ╚");
+            for (int i = 0; i < prompt.Length + 2; i++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("╝ \n");
+            Console.ResetColor();
+        }
+
+        private static void ShowMemberList(List<Member> memberList) // Visar menyn och ber användaren mata in ett värde som sedan skickas tillbaks.
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(" ╔════════════════════════════════════════════════╗ ");
             Console.WriteLine(" ║                 Medlemslista                   ║ ");
             Console.WriteLine(" ╚════════════════════════════════════════════════╝ \n");
             Console.ResetColor();
-            for (int i = 0; i < memberList.Length; i++)
+            for (int i = 0; i < memberList.Count; i++)
             {
                 Console.WriteLine("  Medlem " + memberList[i].ID + ": " + memberList[i].FirstName);
             }
             Console.WriteLine(" ═════════════════════════════════════════════════ \n");
         }
 
-        private static Member GetMember(Member[] memberList)
+        private static Member GetMember(List<Member> memberList) // Hämtar enskild medlem ur medlemslistan.
         {
             int choice = 0;
 
@@ -143,7 +172,7 @@ namespace Program
                 Console.WriteLine(" ║           Välj medlem           ║ ");
                 Console.WriteLine(" ╚═════════════════════════════════╝ \n");
                 Console.ResetColor();
-                for (int i = 0; i < memberList.Length; i++)
+                for (int i = 0; i < memberList.Count; i++)
                 {
                     Console.WriteLine("  Medlem " + memberList[i].ID + ": " + memberList[i].FirstName);
                 }
@@ -155,7 +184,7 @@ namespace Program
                     choice = int.Parse(Console.ReadLine());
                     Console.Clear();
 
-                    if (choice < 1 || choice > memberList.Length)
+                    if (choice < 1 || choice > memberList.Count)
                     {
                         throw new Exception();
                     }
@@ -178,7 +207,7 @@ namespace Program
             return chosenMember;
         }
 
-        private static void ShowMember(Member chosenMember)
+        private static void ShowMember(Member chosenMember) // Visar enskild medlem i detaljerad vy.
         {
             // Variabler för lagring av medlemmens data.
             string firstName = chosenMember.FirstName;
@@ -206,10 +235,59 @@ namespace Program
             }
             Console.Write("╝ \n");
             Console.ResetColor();
-
             Console.WriteLine("\n Namn: " + fullName);
             Console.WriteLine(" Medlemsnummer: " + iD);
             Console.WriteLine(" Telefonnummer: " + phoneNumber);
         }
+
+        private static Member RegisterNewMember(List<Member> memberList)
+        {
+            string fName = null;
+            string lName = null;
+            int pNumber = 0;
+            int identity = memberList.Count + 1;
+
+            do
+            {
+                Console.BackgroundColor = ConsoleColor.DarkCyan;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" ╔══════════════════════════════════════════════╗ ");
+                Console.WriteLine(" ║            Registrera ny medlem              ║ ");
+                Console.WriteLine(" ╚══════════════════════════════════════════════╝ ");
+                Console.ResetColor();
+
+                Console.Write("\n Förnamn: ");
+                fName = Console.ReadLine();
+                Console.Write("\n Efternamn: ");
+                lName = Console.ReadLine();
+                Console.Write("\n Telefonnummer: ");
+
+                try // Ber användaren mata in värden för namn & telefonnummer.
+                {
+                    pNumber = int.Parse(Console.ReadLine());
+                    Console.Clear();
+
+                    if (pNumber < 1 || pNumber > 999999999)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n FEL! Ange endast siffror (utan mellanslag)!\n\n");
+                    Console.ResetColor();
+                }
+            } while (true);
+
+            Member myMember = new Member(fName, lName, pNumber, identity);
+            return myMember;
+        } // Skapa en ny medlem.
     }
 }
