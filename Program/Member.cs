37a20380159d1436835e27de8cfbd37f83a0a368
da <dad21341rafda@dada.com>
2014-01-16 09:52:04 +0100

    Lagt till medlemsfilen igen..

---
+++ Program/Member.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Member
+    {
+        // Fält
+        private string _firstName;
+        private string _lastName;
+        private int _phoneNumber;
+        private int _iD;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public int PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _iD;
+            }
+            set
+            {
+                _iD = value;
+            }
+        }
+
+        // Konstruktorer
+        public Member(string firstName, string lastName, int phoneNumber, int iD)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            ID = iD;
+        }
+    }
+}
